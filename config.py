1f24266ee7e5962643c78f35c5c2cb70a409a6c9
Jon Heston <jonheston@gmail.com>
2018-08-09 20:05:38 -0700

    blah bla

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "Ntzfq9VywWHhP9jOc2G3aBTBw"
+consumer_secret = "SVtfHFdv1HTGe9ejpxhT4Lv8bz9ip0lHIP1vZoGX52IgzDwO41"
+access_token = "4043202390-0pG6FpyzULAAjDg0jKgAr5iNOrS6UAwk8DJlcJx"
+access_token_secret = "zgAIk2ZaafnSrmb1V6WuDwCdzPvDDQVPQx99CUdSzQbmt"
