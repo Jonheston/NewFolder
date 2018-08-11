9fca55cd74e7597cff26d2446c551572726d9109
Jon Heston <jonheston@gmail.com>
2018-08-11 10:25:06 -0700

    final?

--- app-new.py
+++ app-new.py
@@ -15,11 +15,11 @@ import matplotlib
 matplotlib.use("Agg")
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
-# Get config variable from environment variables
-#consumer_key = os.environ.get("consumer_key")
-#consumer_secret = os.environ.get("consumer_secret")
-#access_token = os.environ.get("access_token")
-#access_token_secret = os.environ.get("access_token_secret")
+Get config variable from environment variables
+consumer_key = os.environ.get("consumer_key")
+consumer_secret = os.environ.get("consumer_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
